e1da6b701d61ccb3e4127dbb10e39f1c6ae58bdf
Noman02 <noman.mahib001@gmail.com>
2023-04-08 05:13:07 +0600

    done

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config()
+import dalleRoutes from './routes/dalle.routes.js';
 
-const app= express()
-app.use(cors())
+dotenv.config();
 
-const port = 5000;
+const port =5000;
 
-app.use(express.json({limit: "50mb"}))
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }))
 
-app.get("/",(req,res)=>{
-res.status(200).json({message: "Hello from home"})
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-app.listen(port,()=>{
-    console.log(`server is running on port${port}`)
-})
+app.listen(port, () => console.log(`Server has started on port${port}`))
